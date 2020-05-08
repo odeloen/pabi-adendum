8614324e26cd8a93c03e9576b7a1912be152cbcb
Frandita Adhitama <odeloen.nakmus@gmail.com>
2020-05-08 14:31:57 +0700

    clearing workspace

--- app/ods/elearning/course/presenter/config/route.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::namespace('Web')->middleware(['api'])->group(function () {
-    Route::post("lecturer/courses/create", 'LecturerCourseController@create')->name('test.lecturer.course.create');
-});
